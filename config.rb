a472dafac69efe3839c48d52bbeee024ff10bd14
Eric Meyer <eric@oddbird.net>
2012-05-25 03:48:27 -0600

    outline styles

--- config.rb
+++ config.rb
@@ -9,15 +9,16 @@ css_dir = "css"
 sass_dir = "sass"
 images_dir = "img"
 javascripts_dir = "js"
+fonts_dir = "fonts"
 
 # You can select your preferred output style here (can be overridden via the command line):
-# output_style = :expanded or :nested or :compact or :compressed
+output_style = :expanded
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
-# relative_assets = true
+relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
-# line_comments = false
+line_comments = false
 
 
 # If you prefer the indented syntax, you might want to regenerate this
